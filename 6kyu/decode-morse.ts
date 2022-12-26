ed7a7314cad081d45735a92f39f9ddf4bcb3f720
brishy <vietnemo123@gmail.com>
2022-12-26 13:16:04 +1100

    6kyu

---
+++ 6kyu/decode-morse.ts
@@ -0,0 +1,66 @@
+// The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+
+// NOTE: Extra spaces before or after the code have no meaning and should be ignored.
+
+// In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···. These special codes are treated as single special characters, and usually are transmitted as separate words.
+
+// Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+
+// For example:
+
+// decodeMorse('.... . -.--   .--- ..- -.. .')
+// //should return "HEY JUDE"
+
+// NOTE: For coding purposes you have to use ASCII characters . and -, not Unicode characters.
+
+// The Morse code table is preloaded for you as a dictionary, feel free to use it:
+
+//     Coffeescript/C++/Go/JavaScript/Julia/PHP/Python/Ruby/TypeScript: MORSE_CODE['.--']
+//     C#: MorseCode.Get(".--") (returns string)
+//     F#: MorseCode.get ".--" (returns string)
+//     Elixir: @morse_codes variable (from use MorseCode.Constants). Ignore the unused variable warning for morse_codes because it's no longer used and kept only for old solutions.
+//     Elm: MorseCodes.get : Dict String String
+//     Haskell: morseCodes ! ".--" (Codes are in a Map String String)
+//     Java: MorseCode.get(".--")
+//     Kotlin: MorseCode[".--"] ?: "" or MorseCode.getOrDefault(".--", "")
+//     Racket: morse-code (a hash table)
+//     Rust: MORSE_CODE
+//     Scala: morseCodes(".--")
+//     Swift: MorseCode[".--"] ?? "" or MorseCode[".--", default: ""]
+
+//     C: provides parallel arrays, i.e. morse[2] == "-.-" for ascii[2] == "C"
+
+//     NASM: a table of pointers to the morsecodes, and a corresponding list of ascii symbols
+
+// All the test strings would contain valid Morse code, so you may skip checking for errors and exceptions. In C#, tests will fail if the solution code throws an exception, please keep that in mind. This is mostly because otherwise the engine would simply ignore the tests, resulting in a "valid" solution.
+
+// Good luck!
+
+// import { MORSE_CODE } from "./preloaded";
+
+// We have a special dictionary that translates Morse code into regular letters or numbers.
+const MORSE_CODE: { [key: string]: string } = {
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  // ... (and so on, it maps Morse code sequences to their corresponding letters or numbers)
+};
+
+export function decodeMorse(morseCode: string): string {
+  // We first remove any unnecessary spaces at the beginning or end of the Morse code.
+  // Then we split the Morse code into individual words by looking for three spaces.
+  const words = morseCode.trim().split("   ");
+
+  // Now we're going to translate each word from Morse code to English.
+  // We split each word into individual characters by looking for single spaces.
+  // Then we find the corresponding English letter or number for each Morse code character.
+  // Finally, we join all the characters together to form an English word.
+  return words
+    .map((word) => {
+      const chars = word.split(" ");
+      return chars.map((char) => MORSE_CODE[char]).join("");
+    })
+    .join(" "); // We join all the words together to form the final English message.
+}
