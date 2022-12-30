aa29f2636bb558edafe151598f9b83a82eaa2c43
Tran Hoang Viet <109568688+brishy@users.noreply.github.com>
2022-12-30 22:10:29 +0700

    new 8kyu

---
+++ 8kyu/acsii.js
@@ -0,0 +1,16 @@
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+
+// PREP
+// P: number
+// R: the corresponding ASCII char
+// E: Example:
+
+// get_char(65)
+// should return:
+
+// 'A'
+
+function getChar(num) {
+  return String.fromCharCode(num);
+}
