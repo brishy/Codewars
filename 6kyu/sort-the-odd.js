6cdf08a7d34ea3637e476147c28aa5670eeb5f1c
brishy <vietnemo123@gmail.com>
2023-10-29 23:25:53 +1100

    hey

--- 6kyu/sort-the-odd.js
+++ 6kyu/sort-the-odd.js
@@ -1,4 +1,4 @@
-// // 
+// //
 // Task
 // You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
 
@@ -9,13 +9,9 @@
 //    [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 //    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
-
-
-
-
 function sortArray(arr) {
-    //filter out the even numbers by creating a part copy of the original array then sorting it out
-        const oddArr = arr.filter(x => x%2).sort((a, b) => a - b);
-    //mapping the even numbers into their original places
-        return arr.map(x => x%2 ? oddArr.shift() : x);
-    }
+  //filter out the even numbers by creating a part copy of the original array then sorting it out
+  const oddArr = arr.filter((x) => x % 2).sort((a, b) => a - b);
+  //mapping the even numbers into their original places
+  return arr.map((x) => (x % 2 ? oddArr.shift() : x));
+}
